7e482d67f4310d35cf5d9335918d0cdf0a863714
chenqilin <879646473@qq.com>
2018-12-21 11:24:49 +0800

    初始导入

---
+++ code/fund_catcher/src/main/java/com/kylin/quantization/CatcherMain.java
@@ -0,0 +1,13 @@
+package com.kylin.quantization;
+
+/**
+ * ClassName: CatcherMain
+ * Description:
+ * Author: aierxuan
+ * Date: 2018-12-21 11:23
+ * History:
+ * <author> <time> <version>    <desc>
+ * 作者姓名 修改时间    版本号 描述
+ */
+public class CatcherMain {
+}
---
+++ code/fund_catcher/src/test/java/com/kylin/quantization/TestCenter.java
@@ -0,0 +1,13 @@
+package com.kylin.quantization;
+
+/**
+ * ClassName: TestCenter
+ * Description:
+ * Author: aierxuan
+ * Date: 2018-12-21 11:24
+ * History:
+ * <author> <time> <version>    <desc>
+ * 作者姓名 修改时间    版本号 描述
+ */
+public class TestCenter {
+}
