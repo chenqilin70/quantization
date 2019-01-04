c86066d03ba858b5e3181804a999bd56996990bc
chenqilin70 <879646473@qq.com>
2019-01-04 14:20:59 +0800

    普通提交2

--- code/fund_catcher/src/main/java/com/kylin/quantization/dao/impl/HBaseDaoImpl.java
+++ code/fund_catcher/src/main/java/com/kylin/quantization/dao/impl/HBaseDaoImpl.java
@@ -278,6 +278,8 @@ public class HBaseDaoImpl extends BaseDaoImpl implements HBaseDao{
             if(qs.contains(Bytes.toString(c.getQualifierArray()))){
                 logger.info(Bytes.toString(c.getRowArray())+" | "+Bytes.toString(c.getFamilyArray())+" | "+Bytes.toString(c.getQualifierArray())+" | "+Bytes.toString(c.getValueArray()));
 //                logger.info(Bytes.toString(c.getValueArray()));
+            }else{
+                logger.info(" not contain ==>"+Bytes.toString(c.getQualifierArray()));
             }
         }
 //        logger.info(JSON.toJSONString(cells));
