e800d0acda09c3a452637a6636d624d77e1f1545
chenqilin <879646473@qq.com>
2019-01-04 11:58:56 +0800

    普通提交

--- code/fund_catcher/src/main/java/com/kylin/quantization/computors/NoNetValCodes.java
+++ code/fund_catcher/src/main/java/com/kylin/quantization/computors/NoNetValCodes.java
@@ -1,5 +1,6 @@
 package com.kylin.quantization.computors;
 
+import com.google.common.base.Optional;
 import com.kylin.quantization.config.CatcherConfig;
 import com.kylin.quantization.dao.HBaseDao;
 import com.kylin.quantization.dao.impl.HBaseDaoImpl;
@@ -44,11 +45,9 @@ public class NoNetValCodes extends  BaseSparkMain{
     private static JavaSparkContext  context = new JavaSparkContext(sparkConf());
     public static void main(String[] args) {
         System.out.println("1111111111111111111111111111111111111");
-//        JavaSparkContext netvalcontext = new JavaSparkContext(sparkConf());
         Configuration hconf =getFundListHconf();
         JavaPairRDD<ImmutableBytesWritable, Result> hbaseRdd = context.newAPIHadoopRDD(hconf, TableInputFormat.class, ImmutableBytesWritable.class, Result.class);
-
-        List<Tuple2<String, String>> collect = hbaseRdd.mapToPair(new PairFunction<Tuple2<ImmutableBytesWritable, Result>, String, String>() {
+        JavaPairRDD<String, String> fundRdd = hbaseRdd.mapToPair(new PairFunction<Tuple2<ImmutableBytesWritable, Result>, String, String>() {
             @Override
             public Tuple2<String, String> call(Tuple2<ImmutableBytesWritable, Result> tuple) throws Exception {
 
@@ -56,64 +55,26 @@ public class NoNetValCodes extends  BaseSparkMain{
                 byte[] jjqcArr = tuple._2.getValue(Bytes.toBytes("baseinfo"), Bytes.toBytes("jjqc"));
                 String fundcode = Bytes.toString(fundcodeArr);
                 String jjqc = Bytes.toString(jjqcArr);
-            /*Filter netvalFilter = new RowFilter(CompareFilter.CompareOp.EQUAL, new SubstringComparator("_" + fundcode + "_"));
-            Scan netvalScan = new Scan().setFilter(netvalFilter);
-            String result = hBaseDao.table("netval", nvtable -> {
-                ResultScanner nvScanner = nvtable.getScanner(netvalScan);
-                Result next = nvScanner.next();
-                boolean flag = next == null;
-                nvScanner.close();
-                return flag ? fundcode+"("+jjqc+")" : "";
-            });*/
-                /*Configuration netValHconf = getNetValHconf(fundcode);
-
-                JavaPairRDD<ImmutableBytesWritable, Result> netvalbaseRdd = context.newAPIHadoopRDD(netValHconf, TableInputFormat.class, ImmutableBytesWritable.class, Result.class);*/
-                /*List<Integer> nets = netvalbaseRdd.map(t -> 1).collect();
-                netvalcontext.close();
-                if (nets.size() == 0) {
-                    return fundcode + "(" + jjqc + ")";
-                }*/
-
-
-
-
-
-
-
-
-                Connection conn= hBaseDao.getConn();
-                "d".substring(0,-1);
-                Table table=null;
-                "d".substring(0,-1);
-                String result=null;
-                "d".substring(0,-1);
-                table= conn.getTable(TableName.valueOf("netval"));
-                "d".substring(0,-1);
-                Filter netvalFilter = new RowFilter(CompareFilter.CompareOp.EQUAL, new SubstringComparator("_" + fundcode + "_"));
-                "d".substring(0,-1);
-                Filter pageFilter = new PageFilter(2);
-                "d".substring(0,-1);
-                Scan netvalScan = new Scan().setFilter(new FilterList(FilterList.Operator.MUST_PASS_ALL, pageFilter, netvalFilter));
-                "d".substring(0,-1);
-                ResultScanner netvalscanner = table.getScanner(netvalScan);
-                "d".substring(0,-1);
-                Result next = netvalscanner.next();
-                "d".substring(0,-1);
-                boolean flg = next == null;
-                "d".substring(0,-1);
-                netvalscanner.close();
-                "d".substring(0,-1);
-                if(table!=null){
-                    table.close();
-                }
-
-                return new Tuple2<String, String>(fundcode+"("+jjqc+")", "d" );
+                return new Tuple2<String, String>(fundcode, jjqc);
             }
-        }).collect();
-       /* collect.remove("");*/
-        collect.forEach(c->{
-            System.out.println(c._1()+","+c._2);
         });
+        JavaPairRDD<ImmutableBytesWritable, Result> netvalbaseRdd = context.newAPIHadoopRDD(getNetValHconf(), TableInputFormat.class, ImmutableBytesWritable.class, Result.class);
+        JavaPairRDD<String, String> netvalRdd = netvalbaseRdd.mapToPair(new PairFunction<Tuple2<ImmutableBytesWritable, Result>, String, String>() {
+            @Override
+            public Tuple2<String, String> call(Tuple2<ImmutableBytesWritable, Result> tuple) throws Exception {
+                String rowkey = Bytes.toString(tuple._1().get());
+                String fundcode = rowkey.substring(rowkey.indexOf("_") + 1, rowkey.lastIndexOf("_"));
+                return new Tuple2<String, String>(fundcode, "1");
+            }
+        });
+        JavaPairRDD<String, Tuple2<String, Optional<String>>> joinRdd = fundRdd.leftOuterJoin(netvalRdd);
+        List<Tuple2<String, Tuple2<String, Optional<String>>>> collect = joinRdd.collect();
+        collect.forEach(c->{
+            System.out.println(c._1+":"+c._2._1+":"+c._2._2.get());
+        });
+
+
+
         context.close();
     }
 
@@ -134,9 +95,9 @@ public class NoNetValCodes extends  BaseSparkMain{
         Filter fundcodeFilter=new QualifierFilter(CompareFilter.CompareOp.EQUAL,new BinaryComparator(Bytes.toBytes("fundcode")));
         Filter jjqcFilter=new QualifierFilter(CompareFilter.CompareOp.EQUAL,new BinaryComparator(Bytes.toBytes("jjqc")));
         FilterList conditionList=new FilterList(FilterList.Operator.MUST_PASS_ONE,fundcodeFilter,jjqcFilter);
-        Filter pageFilter=new PageFilter(10);
-        FilterList allList=new FilterList(FilterList.Operator.MUST_PASS_ALL,conditionList,pageFilter);
-        scan.setFilter(allList);
+//        Filter pageFilter=new PageFilter(10);
+//        FilterList allList=new FilterList(FilterList.Operator.MUST_PASS_ALL,conditionList,pageFilter);
+        scan.setFilter(conditionList);
         try {
             hconf.set(TableInputFormat.SCAN, convertScanToString(scan));
         } catch (IOException e) {
@@ -146,14 +107,12 @@ public class NoNetValCodes extends  BaseSparkMain{
     }
 
 
-    private static Configuration getNetValHconf(String fundcode)  {
+    private static Configuration getNetValHconf()  {
         Configuration hconf= HBaseConfiguration.create();
         //需要读取的hbase表名
         String tableName = "netval";
         hconf.set(TableInputFormat.INPUT_TABLE, tableName);
-        Filter netvalFilter=new RowFilter(CompareFilter.CompareOp.EQUAL, new SubstringComparator("_"+fundcode+"_"));
-        Filter pageFilter=new PageFilter(2);
-        Scan netvalScan=new Scan().setFilter(new FilterList(FilterList.Operator.MUST_PASS_ALL,pageFilter,netvalFilter));
+        Scan netvalScan=new Scan();
         try {
             hconf.set(TableInputFormat.SCAN, convertScanToString(netvalScan));
         } catch (IOException e) {
