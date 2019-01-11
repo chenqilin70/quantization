7bffd9698e1e250b1129f9ddb71dbedca1535081
chenqilin <kylinchen85@foxmail.com>
2019-01-11 17:49:37 +0800

    普通提交

--- code/fund_catcher/src/main/java/com/kylin/quantization/computors/FXSort.java
+++ code/fund_catcher/src/main/java/com/kylin/quantization/computors/FXSort.java
@@ -80,7 +80,7 @@ public class FXSort extends BaseSparkMain{
                 SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
                 List<Tuple2<String, BigDecimal>> result = new LinkedList<>();
                 String code = RowKeyUtil.getCodeFromRowkey(tuple._2.getRow());
-                Filter filter2 = new QualifierFilter(CompareFilter.CompareOp.EQUAL, new RegexStringComparator("LJJZ"));
+                /*Filter filter2 = new QualifierFilter(CompareFilter.CompareOp.EQUAL, new RegexStringComparator("LJJZ"));
                 Filter filter3 = new QualifierFilter(CompareFilter.CompareOp.EQUAL, new RegexStringComparator("FSRQ"));
                 FilterList qualifierFilter = new FilterList(FilterList.Operator.MUST_PASS_ONE, filter2, filter3);
                 Scan scan = new Scan().setFilter(qualifierFilter)
@@ -93,7 +93,9 @@ public class FXSort extends BaseSparkMain{
                         Tuple2<String, BigDecimal> t = new Tuple2<>(code, new BigDecimal(Bytes.toString(value)));
                         result.add(t);
                     }
-                });
+                });*/
+                Tuple2<String, BigDecimal> t = new Tuple2<>(code, new BigDecimal("234"));
+                result.add(t);
                 return result;
             }
         });
