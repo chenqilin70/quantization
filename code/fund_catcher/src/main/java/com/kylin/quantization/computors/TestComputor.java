bd4e9abe66b28f968634e60a7fc607312a9048e3
chenqilin <kylinchen85@foxmail.com>
2019-01-11 17:10:35 +0800

    普通提交

--- code/fund_catcher/src/main/java/com/kylin/quantization/computors/TestComputor.java
+++ code/fund_catcher/src/main/java/com/kylin/quantization/computors/TestComputor.java
@@ -1,5 +1,7 @@
 package com.kylin.quantization.computors;
 
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
@@ -19,13 +21,18 @@ import java.util.List;
  */
 public class TestComputor  extends BaseSparkMain{
     public static void main(String[] args) {
+        ImmutableBytesWritable a=new ImmutableBytesWritable(Bytes.toBytes("a"));
+        ImmutableBytesWritable b=new ImmutableBytesWritable(Bytes.toBytes("b"));
+        ImmutableBytesWritable c=new ImmutableBytesWritable(Bytes.toBytes("c"));
+        ImmutableBytesWritable d=new ImmutableBytesWritable(Bytes.toBytes("d"));
+        ImmutableBytesWritable e=new ImmutableBytesWritable(Bytes.toBytes("e"));
         JavaSparkContext context = new JavaSparkContext(sparkConf());
-        List<Tuple2<String ,String>> data = Arrays.asList(new Tuple2<String ,String>("a","1"),new Tuple2<String ,String>("b","2")
-                ,new Tuple2<String ,String>("c","3"),new Tuple2<String ,String>("d","4"),new Tuple2<String ,String>("e","5"));
-        JavaRDD<Tuple2<String ,String>> rdd = context.parallelize(data);
-        List<Tuple2<String, String>> collect = rdd.filter(new Function<Tuple2<String, String>, Boolean>() {
+        List<Tuple2<ImmutableBytesWritable ,String>> data = Arrays.asList(new Tuple2<ImmutableBytesWritable ,String>(a,"1"),new Tuple2<ImmutableBytesWritable ,String>(b,"2")
+                ,new Tuple2<ImmutableBytesWritable ,String>(c,"3"),new Tuple2<ImmutableBytesWritable ,String>(d,"4"),new Tuple2<ImmutableBytesWritable ,String>(e,"5"));
+        JavaRDD<Tuple2<ImmutableBytesWritable ,String>> rdd = context.parallelize(data);
+        List<Tuple2<ImmutableBytesWritable, String>> collect = rdd.filter(new Function<Tuple2<ImmutableBytesWritable, String>, Boolean>() {
             @Override
-            public Boolean call(Tuple2<String, String> v1) throws Exception {
+            public Boolean call(Tuple2<ImmutableBytesWritable, String> v1) throws Exception {
                 if(Integer.parseInt(v1._2)>2){
                     return true;
                 }
@@ -33,7 +40,7 @@ public class TestComputor  extends BaseSparkMain{
             }
         }).collect();
         collect.forEach(t->{
-            System.out.println(t._1+","+t._2);
+            System.out.println(Bytes.toString(t._1.get())+","+t._2);
         });
     }
 }
