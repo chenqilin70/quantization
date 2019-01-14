c34b10dc57f4fdde5e18300832f273ff9b37a54b
chenqilin <kylinchen85@foxmail.com>
2019-01-14 15:22:12 +0800

    普通提交

--- code/fund_catcher/src/main/java/com/kylin/quantization/computors/FXSort.java
+++ code/fund_catcher/src/main/java/com/kylin/quantization/computors/FXSort.java
@@ -104,14 +104,19 @@ public class FXSort extends BaseSparkMain{
         JavaPairRDD<String, BigDecimal> codeValRdd = fundRdd.join(codeNetvalRdd).mapToPair(t -> new Tuple2<String, BigDecimal>(t._1, t._2._2));
 
 
-        JavaPairRDD<String, BigDecimal> sumRdd = codeValRdd.reduceByKey((v1, v2) -> v1.add(v2));
+        /*JavaPairRDD<String, BigDecimal> sumRdd = codeValRdd.reduceByKey((v1, v2) -> v1.add(v2));
         JavaPairRDD<String, BigDecimal> countRdd=codeValRdd.mapToPair(t->new Tuple2<>(t._1,new BigDecimal("1"))).reduceByKey((i,j)->i.add(j));
         JavaPairRDD<String, BigDecimal> avgRdd=sumRdd.join(countRdd).mapToPair(tuple->new Tuple2<>(tuple._1,tuple._2._1.divide(tuple._2._2)));
         JavaPairRDD<String, BigDecimal> eRdd = codeValRdd.join(avgRdd)
                 .mapToPair(tuple -> new Tuple2<String, BigDecimal>(tuple._1, tuple._2._1.subtract(tuple._2._2).pow(2)))
                 .reduceByKey((v1,v2)->v1.add(v2));
         JavaPairRDD<String, BigDecimal> fxRdd = eRdd.leftOuterJoin(countRdd).mapToPair(tuple -> new Tuple2<String, BigDecimal>(tuple._1, tuple._2._1.divide(tuple._2._2.orNull())));
-        List<Tuple2<String, BigDecimal>> collect = fxRdd.collect();
+
+        */
+
+
+
+        List<Tuple2<String, BigDecimal>> collect = codeValRdd.collect();
         logger.info("collect size:"+collect.size());
         collect.forEach(t->{
             logger.info("code:"+t._1+",fx:"+t._2);
