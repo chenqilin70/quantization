c1408d2dad789914784efd4195d90c71fd65848f
chenqilin <kylinchen85@foxmail.com>
2019-01-18 17:55:57 +0800

    普通提交

--- code/fund_catcher/src/main/java/com/kylin/quantization/computors/BaseSparkMain.java
+++ code/fund_catcher/src/main/java/com/kylin/quantization/computors/BaseSparkMain.java
@@ -86,7 +86,6 @@ public abstract class BaseSparkMain {
                     setMethodName="set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
                     paramClass=f.getType();
                     setMethodParam=transTypeFromString(ResultUtil.strVal(result, "baseinfo", fieldName),paramClass);
-//                    System.out.println("setMethodName:"+setMethodName);
 
                 }
                 Method setMethod = clazz.getMethod(setMethodName, paramClass);
