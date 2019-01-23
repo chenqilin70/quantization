e3514e1a2b85ffa42db5f5a9ab16f504d40c5ce3
chenqilin <kylinchen85@foxmail.com>
2019-01-23 11:48:01 +0800

    普通提交

--- code/fund_catcher/src/main/java/com/kylin/quantization/component/ReportRunner.java
+++ code/fund_catcher/src/main/java/com/kylin/quantization/component/ReportRunner.java
@@ -2,7 +2,9 @@ package com.kylin.quantization.component;
 
 import com.kylin.quantization.service.CatcherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ReportRunner   extends CatcherRunner {
     @Autowired
     private CatcherService service;
