a34a726847e3db8040e765e395c8d3cae0b6b37b
chenqilin70 <879646473@qq.com>
2019-02-13 16:37:48 +0800

    普通提交2

--- code/fund_catcher/src/main/scala/com/kylin/quantization/MovingAveragesMain.scala
+++ code/fund_catcher/src/main/scala/com/kylin/quantization/MovingAveragesMain.scala
@@ -3,11 +3,13 @@ package com.kylin.quantization
 import com.kylin.quantization.KernalMain.{SQL_TAB, sparkConf, sql}
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.hive.HiveContext
 
 object MovingAveragesMain extends ScalaBaseSparkMain{
   def main(args: Array[String]): Unit = {
     var sparkContext =new SparkContext(sparkConf())
-    var sqlContext=new SQLContext(sparkContext)
+    var sqlContext=new HiveContext(sparkContext)
+
     var df=sql("movingAverages",sparkContext,sqlContext)
     df.rdd.collect().foreach(r=>{
       println(r.toString())
