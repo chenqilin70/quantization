645859c24220028920e1e4179531dc0f32185eef
chenqilin <kylinchen85@foxmail.com>
2019-02-17 11:39:46 +0800

    普通提交

--- code/fund_catcher/src/main/scala/com/kylin/quantization/util/HbaseToHiveConverter.scala
+++ code/fund_catcher/src/main/scala/com/kylin/quantization/util/HbaseToHiveConverter.scala
@@ -2,6 +2,7 @@ package com.kylin.quantization.util
 
 import java.io.IOException
 
+import com.kylin.quantization.CatcherMain
 import com.kylin.quantization.computors.BaseSparkMain
 import com.kylin.quantization.config.CatcherConfig
 import com.kylin.quantization.dao.impl.{HBaseDaoImpl, HBaseExecutors}
@@ -10,6 +11,8 @@ import org.apache.hadoop.hbase.client.{Admin, Result, Scan, Table}
 import org.apache.hadoop.hbase.filter._
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat
 import org.apache.hadoop.hbase.util.Bytes
+import org.apache.log4j.PropertyConfigurator
+import org.slf4j.LoggerFactory
 
 /**
   * ClassName: HbaseToHiveConverter
@@ -21,6 +24,11 @@ import org.apache.hadoop.hbase.util.Bytes
   * 作者姓名 修改时间    版本号 描述
   */
 object HbaseToHiveConverter {
+  val logger= LoggerFactory.getLogger(HbaseToHiveConverter.getClass);
+  {
+    PropertyConfigurator.configure(HbaseToHiveConverter.getClass.getClassLoader.getResourceAsStream("log4j_linux.properties"))
+    logger.info("使用日志配置文件:log4j_linux.properties")
+  }
   def main(args: Array[String]): Unit = {
     convert()
   }
@@ -34,13 +42,13 @@ object HbaseToHiveConverter {
           var scan=new Scan()
           var filter=new PageFilter(1);
           scan.setFilter(filter)
-          println("======================="+tableName)
+          logger.info("======================="+tableName)
           dao.scanForEach(tableName,scan,new HBaseExecutors.ScanForEachExecutor {
             override def doEach(result: Result): Unit = {
               var keyset=result.getMap.keySet()
               var it=keyset.iterator()
               while(it.hasNext){
-                println(Bytes.toString(it.next()))
+                logger.info(Bytes.toString(it.next()))
               }
             }
           })
