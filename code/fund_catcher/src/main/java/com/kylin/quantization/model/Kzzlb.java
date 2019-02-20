bfb628d812a384ac159e434190f6f6a9a7dbd916
chenqilin70 <879646473@qq.com>
2019-02-20 10:13:10 +0800

    提交集群测试

---
+++ code/fund_catcher/src/main/java/com/kylin/quantization/model/Kzzlb.java
@@ -0,0 +1,292 @@
+package com.kylin.quantization.model;
+
+public class Kzzlb {
+    private String key			;
+    private String aissuevol		;
+    private String bcode			;
+    private String bondcode			;
+    private String correscode		;
+    private String corresname		;
+    private String delistdate		;
+    private String fstplacvalperstk		;
+    private String gdyx_startdate		;
+    private String hscfj			;
+    private String issueprice		;
+    private String limitbuyipub		;
+    private String listdate			;
+    private String luckrate			;
+    private String memo			;
+    private String markettype		;
+    private String parvalue			;
+    private String pb			;
+    private String qscfj			;
+    private String securityshortname	;
+    private String sname			;
+    private String startdate		;
+    private String swapprice		;
+    private String swapscode		;
+    private String texch			;
+    private String yjl			;
+    private String zgj			;
+    private String zgjzgj			;
+    private String zgjzgjjz			;
+    private String zgj_hq			;
+    private String zqhdate			;
+    private String zqnew			;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getAissuevol() {
+        return aissuevol;
+    }
+
+    public void setAissuevol(String aissuevol) {
+        this.aissuevol = aissuevol;
+    }
+
+    public String getBcode() {
+        return bcode;
+    }
+
+    public void setBcode(String bcode) {
+        this.bcode = bcode;
+    }
+
+    public String getBondcode() {
+        return bondcode;
+    }
+
+    public void setBondcode(String bondcode) {
+        this.bondcode = bondcode;
+    }
+
+    public String getCorrescode() {
+        return correscode;
+    }
+
+    public void setCorrescode(String correscode) {
+        this.correscode = correscode;
+    }
+
+    public String getCorresname() {
+        return corresname;
+    }
+
+    public void setCorresname(String corresname) {
+        this.corresname = corresname;
+    }
+
+    public String getDelistdate() {
+        return delistdate;
+    }
+
+    public void setDelistdate(String delistdate) {
+        this.delistdate = delistdate;
+    }
+
+    public String getFstplacvalperstk() {
+        return fstplacvalperstk;
+    }
+
+    public void setFstplacvalperstk(String fstplacvalperstk) {
+        this.fstplacvalperstk = fstplacvalperstk;
+    }
+
+    public String getGdyx_startdate() {
+        return gdyx_startdate;
+    }
+
+    public void setGdyx_startdate(String gdyx_startdate) {
+        this.gdyx_startdate = gdyx_startdate;
+    }
+
+    public String getHscfj() {
+        return hscfj;
+    }
+
+    public void setHscfj(String hscfj) {
+        this.hscfj = hscfj;
+    }
+
+    public String getIssueprice() {
+        return issueprice;
+    }
+
+    public void setIssueprice(String issueprice) {
+        this.issueprice = issueprice;
+    }
+
+    public String getLimitbuyipub() {
+        return limitbuyipub;
+    }
+
+    public void setLimitbuyipub(String limitbuyipub) {
+        this.limitbuyipub = limitbuyipub;
+    }
+
+    public String getListdate() {
+        return listdate;
+    }
+
+    public void setListdate(String listdate) {
+        this.listdate = listdate;
+    }
+
+    public String getLuckrate() {
+        return luckrate;
+    }
+
+    public void setLuckrate(String luckrate) {
+        this.luckrate = luckrate;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+
+    public String getMarkettype() {
+        return markettype;
+    }
+
+    public void setMarkettype(String markettype) {
+        this.markettype = markettype;
+    }
+
+    public String getParvalue() {
+        return parvalue;
+    }
+
+    public void setParvalue(String parvalue) {
+        this.parvalue = parvalue;
+    }
+
+    public String getPb() {
+        return pb;
+    }
+
+    public void setPb(String pb) {
+        this.pb = pb;
+    }
+
+    public String getQscfj() {
+        return qscfj;
+    }
+
+    public void setQscfj(String qscfj) {
+        this.qscfj = qscfj;
+    }
+
+    public String getSecurityshortname() {
+        return securityshortname;
+    }
+
+    public void setSecurityshortname(String securityshortname) {
+        this.securityshortname = securityshortname;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getSwapprice() {
+        return swapprice;
+    }
+
+    public void setSwapprice(String swapprice) {
+        this.swapprice = swapprice;
+    }
+
+    public String getSwapscode() {
+        return swapscode;
+    }
+
+    public void setSwapscode(String swapscode) {
+        this.swapscode = swapscode;
+    }
+
+    public String getTexch() {
+        return texch;
+    }
+
+    public void setTexch(String texch) {
+        this.texch = texch;
+    }
+
+    public String getYjl() {
+        return yjl;
+    }
+
+    public void setYjl(String yjl) {
+        this.yjl = yjl;
+    }
+
+    public String getZgj() {
+        return zgj;
+    }
+
+    public void setZgj(String zgj) {
+        this.zgj = zgj;
+    }
+
+    public String getZgjzgj() {
+        return zgjzgj;
+    }
+
+    public void setZgjzgj(String zgjzgj) {
+        this.zgjzgj = zgjzgj;
+    }
+
+    public String getZgjzgjjz() {
+        return zgjzgjjz;
+    }
+
+    public void setZgjzgjjz(String zgjzgjjz) {
+        this.zgjzgjjz = zgjzgjjz;
+    }
+
+    public String getZgj_hq() {
+        return zgj_hq;
+    }
+
+    public void setZgj_hq(String zgj_hq) {
+        this.zgj_hq = zgj_hq;
+    }
+
+    public String getZqhdate() {
+        return zqhdate;
+    }
+
+    public void setZqhdate(String zqhdate) {
+        this.zqhdate = zqhdate;
+    }
+
+    public String getZqnew() {
+        return zqnew;
+    }
+
+    public void setZqnew(String zqnew) {
+        this.zqnew = zqnew;
+    }
+}
