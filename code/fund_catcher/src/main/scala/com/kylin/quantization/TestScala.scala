9085843e63ff2d48340f3227002b7cec6a864146
chenqilin <kylinchen85@foxmail.com>
2019-02-22 18:33:48 +0800

    普通提交

--- code/fund_catcher/src/main/scala/com/kylin/quantization/TestScala.scala
+++ code/fund_catcher/src/main/scala/com/kylin/quantization/TestScala.scala
@@ -21,11 +21,28 @@ import redis.clients.jedis.Jedis
 object TestScala  extends ScalaBaseSparkMain{
   def main(args: Array[String]): Unit = {
 
-    val v1 = Vectors.dense(43.0, 9.0)
+    val v1 = Vectors.dense(1.0,2.0 ,3.0 ,4.0 ,5.0 )
 
     val v2 = Vectors.dense(44.0, 4.0)
 
-    val c1 = Statistics.chiSqTest(v1, v2)
+    val c1 = Statistics.chiSqTest(v1)
+    /**
+      * method: 方法。这里采用pearson方法。
+
+        statistic：
+          检验统计量。简单来说就是用来决定是否可以拒绝原假设的证据。检验统计量的值是利用样本数据计算得到的，
+          它代表了样本中的信息。检验统计量的绝对值越大，拒绝原假设的理由越充分，反之，不拒绝原假设的理由越充分。
+
+        degrees of freedom：
+          自由度。表示可自由变动的样本观测值的数目，
+
+        pValue：
+          统计学根据显著性检验方法所得到的P 值。一般以P < 0.05 为显著， P<0.01 为非常显著，
+          其含义是样本间的差异由抽样误差所致的概率小于0.05 或0.01。
+
+        一般来说，假设检验主要看P值就够了。
+      */
+    println(c1)
   }
 
   override def getCustomHbaseConf(): Map[String, Configuration] = {
