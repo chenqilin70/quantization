5676c499dce3933e143247fd45d5bd918056b350
chenqilin70 <kylinchen85@foxmail.com>
2019-02-25 11:23:31 +0800

    提交集群测试

--- code/fund_catcher/src/main/scala/com/kylin/quantization/SparkMllibTest.scala
+++ code/fund_catcher/src/main/scala/com/kylin/quantization/SparkMllibTest.scala
@@ -13,35 +13,42 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.SparkContext
 import org.apache.spark.mllib.random.RandomRDDs._
 
+
+
 object SparkMllibTest extends ScalaBaseSparkMain{
   def main(args: Array[String]): Unit = {
     var sc=new SparkContext(sparkConf())
-    val u = normalRDD(sc, 100000L, 10)//生成100个服从标准正态分面N(0,1)的随机RDD数据，10为指定的分区数
+    val u = normalRDD(sc, 10000L, 10)//生成100个服从标准正态分面N(0,1)的随机RDD数据，10为指定的分区数
 
     val v = u.map(x => 1.0 + 2.0 * x)//转换使其服从N(1,4)的正太分布
 
 
     var result=List[Map[String,Object]]();
 
-    var map1=Map("type" -> "line","data" -> {
-      var ucollect=u.collect()
-      var ulist=List[List[Any]]()
-      for(i <- Range(0,ucollect.size)){
-        ulist=ulist :+ List(i,ucollect(i))
-      }
-      ulist
-    })
-    var map2=Map("type" -> "line","data" -> {
-      var vcollect=v.collect()
-      var vlist=List[List[Any]]()
-      for(i <- Range(0,vcollect.size)){
-        vlist=vlist :+ List(i,vcollect(i))
-      }
-      vlist
-    })
+
+    var ucollect=u.collect()
+    var ulist=List[List[Any]]()
+    for(i <- Range(0,ucollect.size)){
+      ulist=ulist :+ List(i,ucollect(i))
+    }
+    println(ulist)
+    var map1=Map("type" -> "line","data" -> ulist)
+
+    var vcollect=v.collect()
+    var vlist=List[List[Any]]()
+    for(i <- Range(0,vcollect.size)){
+      vlist=vlist :+ List(i,vcollect(i))
+    }
+    println(vlist)
+    var map2=Map("type" -> "line","data" -> vlist)
+
+
+
+
     result=result :+ (map1)
     result=result :+ (map2)
     var resultJson=JSON.toJSONString(result,SerializerFeature.PrettyFormat)
+
     println(resultJson)
     JedisUtil.set("lineSeries",resultJson)
     JedisUtil.destroy()
