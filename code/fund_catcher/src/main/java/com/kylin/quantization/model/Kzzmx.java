810a58098473c7804e0b5a89ddda2ce028e54f7b
chenqilin70 <kylinchen85@foxmail.com>
2019-02-27 14:41:38 +0800

    提交集群测试

---
+++ code/fund_catcher/src/main/java/com/kylin/quantization/model/Kzzmx.java
@@ -0,0 +1,311 @@
+package com.kylin.quantization.model;
+
+public class Kzzmx {
+    private String rowkey;
+    private String BCODE               ;
+    private String SNAME		   ;
+    private String OLDCORRESNAME	   ;
+    private String LASTVALUEDATE	   ;
+    private String PAYDAY		   ;
+    private String PARTIESFNAME	   ;
+    private String LIMITBUYIPUB	   ;
+    private String SWAPSDATE	   ;
+    private String FRSTVALUEDATE	   ;
+    private String OLDCORRESCODE	   ;
+    private String MEMO		   ;
+    private String SWAPEDATE	   ;
+    private String LUCKRATE		   ;
+    private String ISSUEOBJECT	   ;
+    private String LISTDATE		   ;
+    private String PARVALUE		   ;
+    private String SWAPSCODE	   ;
+    private String MRTYDATE		   ;
+    private String CORRESCODE	   ;
+    private String STARTDATE	   ;
+    private String ISSUEPRICE	   ;
+    private String CREDITRATING	   ;
+    private String ZQHDATE		   ;
+    private String TEXCH		   ;
+    private String BONDCODE		   ;
+    private String RATEDES		   ;
+    private String SECURITYSHORTNAME   ;
+    private String ISSUEYEAR	   ;
+    private String AISSUEVOL	   ;
+    private String SWAPPRICE	   ;
+    private String DELISTDATE	   ;
+    private String BONDPERIOD	   ;
+    private String CORRESNAME	   ;
+
+
+    public String getRowkey() {
+        return rowkey;
+    }
+
+    public void setRowkey(String rowkey) {
+        this.rowkey = rowkey;
+    }
+
+    public String getBCODE() {
+        return BCODE;
+    }
+
+    public void setBCODE(String BCODE) {
+        this.BCODE = BCODE;
+    }
+
+    public String getSNAME() {
+        return SNAME;
+    }
+
+    public void setSNAME(String SNAME) {
+        this.SNAME = SNAME;
+    }
+
+    public String getOLDCORRESNAME() {
+        return OLDCORRESNAME;
+    }
+
+    public void setOLDCORRESNAME(String OLDCORRESNAME) {
+        this.OLDCORRESNAME = OLDCORRESNAME;
+    }
+
+    public String getLASTVALUEDATE() {
+        return LASTVALUEDATE;
+    }
+
+    public void setLASTVALUEDATE(String LASTVALUEDATE) {
+        this.LASTVALUEDATE = LASTVALUEDATE;
+    }
+
+    public String getPAYDAY() {
+        return PAYDAY;
+    }
+
+    public void setPAYDAY(String PAYDAY) {
+        this.PAYDAY = PAYDAY;
+    }
+
+    public String getPARTIESFNAME() {
+        return PARTIESFNAME;
+    }
+
+    public void setPARTIESFNAME(String PARTIESFNAME) {
+        this.PARTIESFNAME = PARTIESFNAME;
+    }
+
+    public String getLIMITBUYIPUB() {
+        return LIMITBUYIPUB;
+    }
+
+    public void setLIMITBUYIPUB(String LIMITBUYIPUB) {
+        this.LIMITBUYIPUB = LIMITBUYIPUB;
+    }
+
+    public String getSWAPSDATE() {
+        return SWAPSDATE;
+    }
+
+    public void setSWAPSDATE(String SWAPSDATE) {
+        this.SWAPSDATE = SWAPSDATE;
+    }
+
+    public String getFRSTVALUEDATE() {
+        return FRSTVALUEDATE;
+    }
+
+    public void setFRSTVALUEDATE(String FRSTVALUEDATE) {
+        this.FRSTVALUEDATE = FRSTVALUEDATE;
+    }
+
+    public String getOLDCORRESCODE() {
+        return OLDCORRESCODE;
+    }
+
+    public void setOLDCORRESCODE(String OLDCORRESCODE) {
+        this.OLDCORRESCODE = OLDCORRESCODE;
+    }
+
+    public String getMEMO() {
+        return MEMO;
+    }
+
+    public void setMEMO(String MEMO) {
+        this.MEMO = MEMO;
+    }
+
+    public String getSWAPEDATE() {
+        return SWAPEDATE;
+    }
+
+    public void setSWAPEDATE(String SWAPEDATE) {
+        this.SWAPEDATE = SWAPEDATE;
+    }
+
+    public String getLUCKRATE() {
+        return LUCKRATE;
+    }
+
+    public void setLUCKRATE(String LUCKRATE) {
+        this.LUCKRATE = LUCKRATE;
+    }
+
+    public String getISSUEOBJECT() {
+        return ISSUEOBJECT;
+    }
+
+    public void setISSUEOBJECT(String ISSUEOBJECT) {
+        this.ISSUEOBJECT = ISSUEOBJECT;
+    }
+
+    public String getLISTDATE() {
+        return LISTDATE;
+    }
+
+    public void setLISTDATE(String LISTDATE) {
+        this.LISTDATE = LISTDATE;
+    }
+
+    public String getPARVALUE() {
+        return PARVALUE;
+    }
+
+    public void setPARVALUE(String PARVALUE) {
+        this.PARVALUE = PARVALUE;
+    }
+
+    public String getSWAPSCODE() {
+        return SWAPSCODE;
+    }
+
+    public void setSWAPSCODE(String SWAPSCODE) {
+        this.SWAPSCODE = SWAPSCODE;
+    }
+
+    public String getMRTYDATE() {
+        return MRTYDATE;
+    }
+
+    public void setMRTYDATE(String MRTYDATE) {
+        this.MRTYDATE = MRTYDATE;
+    }
+
+    public String getCORRESCODE() {
+        return CORRESCODE;
+    }
+
+    public void setCORRESCODE(String CORRESCODE) {
+        this.CORRESCODE = CORRESCODE;
+    }
+
+    public String getSTARTDATE() {
+        return STARTDATE;
+    }
+
+    public void setSTARTDATE(String STARTDATE) {
+        this.STARTDATE = STARTDATE;
+    }
+
+    public String getISSUEPRICE() {
+        return ISSUEPRICE;
+    }
+
+    public void setISSUEPRICE(String ISSUEPRICE) {
+        this.ISSUEPRICE = ISSUEPRICE;
+    }
+
+    public String getCREDITRATING() {
+        return CREDITRATING;
+    }
+
+    public void setCREDITRATING(String CREDITRATING) {
+        this.CREDITRATING = CREDITRATING;
+    }
+
+    public String getZQHDATE() {
+        return ZQHDATE;
+    }
+
+    public void setZQHDATE(String ZQHDATE) {
+        this.ZQHDATE = ZQHDATE;
+    }
+
+    public String getTEXCH() {
+        return TEXCH;
+    }
+
+    public void setTEXCH(String TEXCH) {
+        this.TEXCH = TEXCH;
+    }
+
+    public String getBONDCODE() {
+        return BONDCODE;
+    }
+
+    public void setBONDCODE(String BONDCODE) {
+        this.BONDCODE = BONDCODE;
+    }
+
+    public String getRATEDES() {
+        return RATEDES;
+    }
+
+    public void setRATEDES(String RATEDES) {
+        this.RATEDES = RATEDES;
+    }
+
+    public String getSECURITYSHORTNAME() {
+        return SECURITYSHORTNAME;
+    }
+
+    public void setSECURITYSHORTNAME(String SECURITYSHORTNAME) {
+        this.SECURITYSHORTNAME = SECURITYSHORTNAME;
+    }
+
+    public String getISSUEYEAR() {
+        return ISSUEYEAR;
+    }
+
+    public void setISSUEYEAR(String ISSUEYEAR) {
+        this.ISSUEYEAR = ISSUEYEAR;
+    }
+
+    public String getAISSUEVOL() {
+        return AISSUEVOL;
+    }
+
+    public void setAISSUEVOL(String AISSUEVOL) {
+        this.AISSUEVOL = AISSUEVOL;
+    }
+
+    public String getSWAPPRICE() {
+        return SWAPPRICE;
+    }
+
+    public void setSWAPPRICE(String SWAPPRICE) {
+        this.SWAPPRICE = SWAPPRICE;
+    }
+
+    public String getDELISTDATE() {
+        return DELISTDATE;
+    }
+
+    public void setDELISTDATE(String DELISTDATE) {
+        this.DELISTDATE = DELISTDATE;
+    }
+
+    public String getBONDPERIOD() {
+        return BONDPERIOD;
+    }
+
+    public void setBONDPERIOD(String BONDPERIOD) {
+        this.BONDPERIOD = BONDPERIOD;
+    }
+
+    public String getCORRESNAME() {
+        return CORRESNAME;
+    }
+
+    public void setCORRESNAME(String CORRESNAME) {
+        this.CORRESNAME = CORRESNAME;
+    }
+}
---
+++ code/fund_catcher/src/main/scala/com/kylin/quantization/KzzSelect.scala
@@ -0,0 +1,39 @@
+package com.kylin.quantization
+
+import com.kylin.quantization.computors.BaseSparkMain
+import com.kylin.quantization.model.Kzzmx
+import com.kylin.quantization.util.JedisUtil
+import org.apache.hadoop.conf.Configuration
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.random.RandomRDDs._
+import org.apache.spark.sql.SQLContext
+import org.json4s.NoTypeHints
+import org.json4s.jackson.Serialization
+import org.json4s.jackson.Serialization._
+
+
+object SparkMllibTest extends ScalaBaseSparkMain{
+  def main(args: Array[String]): Unit = {
+    var sc=new SparkContext(sparkConf())
+    var sqlSc=new SQLContext(sc)
+    var df=sql("kzz_select1",sc,sqlSc)
+    df.show(20)
+    var javaRdds=df.map(d=>{
+      var kzzmx=new Kzzmx()
+      kzzmx.setRowkey(d.getString(0))
+      kzzmx.setBONDCODE(d.getString(1))
+      kzzmx.setRATEDES(d.getString(2))
+      kzzmx.setMRTYDATE(d.getString(3))
+      kzzmx
+    })
+    var worked_kzzmx=sqlSc.createDataFrame(javaRdds,classOf[Kzzmx])
+    BaseSparkMain.registerHbaseTable("worked_kzzmx",worked_kzzmx)
+
+
+    var df2=sql("kzz_select2",sc,sqlSc)
+    df2.show(20)
+    sc.stop()
+  }
+
+  override def getCustomHbaseConf(): Map[String, Configuration] = {null}
+}
